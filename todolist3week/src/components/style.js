111eea1c5a15c5a2e9b9123e6f8cf204e7811a42
abxl1 <etho_818@naver.com>
2023-07-11 22:28:59 +0900

    컴포넌트 간의 간격

--- todolist3week/src/components/style.js
+++ todolist3week/src/components/style.js
@@ -1,4 +1,9 @@
 import { styled } from 'styled-components';
+// export const  = styled.div``
+
+export const Align = styled.div`
+  margin-bottom: 40px;
+`
 
 export const Form = styled.form`
   display: flex;
@@ -23,12 +28,20 @@ export const FormSaveBtn = styled.button`
   color: #000;
   width: 100px;
   height: 40px;
+  cursor: pointer;
 `
 
 export const H1 = styled.h1`
   margin-left: 10px;
   font-weight: 700;
+  padding-left: 0px;
 `
+
+export const SelectContainer = styled.div`
+  border: 3px solid gray;
+  margin: 10px;
+`
+
 
 export const SelectZone = styled.div`
   border: 3px solid gray;
@@ -55,4 +68,39 @@ export const SelectBtn = styled.button`
   width: 250px;
   height: 30px;
   background-color: #fff;
+  cursor: pointer;
+
+  overflow: hidden; // 버튼 내 텍스트 말줄임표
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+export const SelectOpen = styled.div`
+  margin: 10px 10px;
+`
+
+export const Wrap = styled.div`
+  border: 1px solid lightgrey;
+  height: 200px;
+  margin: 10px;
+`
+
+export const WrapSelecBtn = styled.div`
+  display: flex;
+`
+
+export const SelecPosition = styled.div`
+  margin-top: 10px;
+  border: 1px solid lightgrey;
+  border-radius: 8px;
+  background-color: #fff;
+  overflow: hidden;
+  width: 250px;
+    div {
+      overflow: hidden; // 옵션리스트 내 말줄임표
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      padding: 8px;
+      &:hover {background-color: lightgrey;}
+    }
 `
