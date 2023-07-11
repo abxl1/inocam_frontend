e4aadd7a1bdef31cfaacf6f2595ff0e161c3a981
abxl1 <etho_818@naver.com>
2023-07-11 22:26:15 +0900

    전체적인 변수명 변경, Select 구현

--- todolist3week/src/components/Select.jsx
+++ todolist3week/src/components/Select.jsx
@@ -1,65 +1,53 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as s from './style';
+// import { styled } from 'styled-components';
 
 
-function Select() {
+function Select({position}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('리액트');
+  const options = ['자바', '스프링', '리액트', '자바스크립트', '리액트 너무 재미있어요~!~!~!~!~!~!~!~!~!~!~~!~~']; 
 
-  // 옵션의 스프레드 여부 상태와 옵션 선택의 상태
-  const [ isOptionSpread, setIsOptionSpread ] = useState(false);
-  const [ selectedOption, setSelectedOption ] = useState('');
+  const selectRef = useRef(); 
 
-  // 옵션 스프레드(버튼 클릭시) 렌더링 방지
-  const selectRef = useRef(null);
+  const handleOptionClick = (i) => { // 옵션을 선택하면 실행
+    setSelectedOption(options[i]);
+    setIsOpen(!isOpen); // 리스트 상태 반대로
+  };
 
-  const options = ['리액트', '자바', '스프링', '자바스크립트'];
-
-  const selectClickHandler = () => {
-    setIsOptionSpread(!isOptionSpread);
-  }
-
-  const optionClickHandler = (option) => {
-    setSelectedOption(option);
-    setIsOptionSpread(false);
-  }
-  
+  // 클릭 이벤트 리스너 등록 - 최상위 부모에 입력
   useEffect(() => {
-    const clickOuter = (event) => {
-      if (isOptionSpread && selectRef.current && !selectRef.current.contain(event.tartget)) {setIsOptionSpread(false);}
-    };
-  
-    document.addEventListener('mousedown', clickOuter);
 
-    return () => {
-      document.removeEventListener('mousedown', clickOuter);
-    }
-  }, [isOptionSpread]);
+    const handleClickOutside = (event) => { // 옵션의 바깥을 누르면 실행
+      if (selectRef.current && !selectRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('click', handleClickOutside); // select가 열리면(마운트되면) 동작
+    return () => { // 새로고침되는 순간에 동작!
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
     
 
-  return (
+  return ( 
+    // {원하는 거.length > 원하는 길이 ? `${원하는거.slice(0, 원하는 길이)}...` : 원하는 거}
     <>
-      <s.SelectZone>
-        <s.H1>Select</s.H1>
-          <s.SelectWrapper>
-            <s.SelectAlign>
-              <s.SelectBtn ref={selectRef} onClick={selectClickHandler}>
-                <option value={options[0]}>리액트</option>
-                <option value={options[1]}>자바</option>
-                <option value={options[2]}>스프링</option>
-                <option value={options[3]}>자바스크립트</option>
-                <div>▼</div>
-              </s.SelectBtn>
-            </s.SelectAlign>
-            <s.SelectAlign>
-            <s.SelectBtn ref={selectRef} onClick={selectClickHandler}>
-                <option value={options[0]}>리액트</option>
-                <option value={options[1]}>자바</option>
-                <option value={options[2]}>스프링</option>
-                <option value={options[3]}>자바스크립트</option>
-                <div>▼</div>
-              </s.SelectBtn>
-            </s.SelectAlign>
-          </s.SelectWrapper>
-        </s.SelectZone>
+      <s.SelectOpen ref={selectRef}>
+        <s.SelectBtn className="selected-option" onClick={()=>{setIsOpen(!isOpen)}}>
+          {selectedOption}<div>▼</div>
+        </s.SelectBtn>
+            {isOpen && (<s.SelecPosition position={position}>
+                {options.map((option, i) => {
+                  return <div
+                    key={i}
+                    onClick={() => handleOptionClick(i)}>
+                    {option}
+                  </div>
+                })}
+              </s.SelecPosition>)
+            }
+      </s.SelectOpen>
     </>
   )
 }
