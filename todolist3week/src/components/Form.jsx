67132d0b06932b6efd88d7778fea5bc509b60cc1
abxl1 <etho_818@naver.com>
2023-07-11 22:25:36 +0900

    메서드 체이닝을 위한 변수 제거, 메서드 체이닝

--- todolist3week/src/components/Form.jsx
+++ todolist3week/src/components/Form.jsx
@@ -14,9 +14,10 @@ function Form() {
   
   // 가격 변경
   const inputMuchChange = (event) => {
-    const much = event.target.value.replace(/[^0-9]/g, '') // only 숫자로 제어
-    const nextMuch = much.replace(/\B(?=(\d{3})+(?!\d))/g, ",") // 3자리마다 (,) 추가
-    setInputMuch(nextMuch)
+    const much = event.target.value
+                      .replace(/[^0-9]/g, '') // only 숫자로 제어
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",") // 3자리마다 (,) 추가
+    setInputMuch(much)
   }
 
   // 저장 버튼, input 2개 모두 입력 필요, 가격은 (,) 제외 출력
@@ -25,27 +26,26 @@ function Form() {
       alert("두 값을 모두 입력해 주세요")
     } else {
       alert(`{ name: ${inputName}, price: ${inputMuch.replace(/,/g, '')} }`) // (,)는 제거 후 출력
-    }
+    }    
   }
 
   return (
-    <>
-    <s.H1>input</s.H1>
-      <s.Form>
-        <s.FormInput>
-          <label>이름</label>
-          <input type="text" value={inputName} onChange={inputNameChange}/>
-        </s.FormInput>
-        <s.FormInput>
-          <label>가격</label>
-          <input type="text" value={inputMuch} onChange={inputMuchChange}/>
-        </s.FormInput>
-        <s.FormSaveBtn onClick={saveBtnEvent}>
-        저장
-        </s.FormSaveBtn>
-      </s.Form>
-    
-    </>
+    <s.Align>
+      <s.H1>input</s.H1>
+        <s.Form>
+          <s.FormInput>
+            <label>이름</label>
+            <input placeholder='이름을 입력해 주세용' type="text" value={inputName} onChange={inputNameChange}/>
+          </s.FormInput>
+          <s.FormInput>
+            <label>가격</label>
+            <input placeholder='0' type="text" value={inputMuch} onChange={inputMuchChange}/>
+          </s.FormInput>
+          <s.FormSaveBtn onClick={saveBtnEvent}>
+          저장
+          </s.FormSaveBtn>
+        </s.Form>
+    </s.Align>
   )
 }
 
