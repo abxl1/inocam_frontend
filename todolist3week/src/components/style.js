935e6dc6fcf2038eebfe028f3c1129bc0726bd08
abxl1 <etho_818@naver.com>
2023-07-12 09:47:24 +0900

    미사용 CSS 수정, 조건부 스타일링 상위 요소 수정

--- todolist3week/src/components/style.js
+++ todolist3week/src/components/style.js
@@ -53,40 +53,39 @@ export const SelectBtn = styled.button`
   white-space: nowrap;
 `
 
-export const OverControl = styled.div`
-  width: 250px;
-  overflow: hidden; // 버튼 내 텍스트 말줄임표
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  display: flex;
-`
-
-export const Wrap = styled.div`
-  border: 1px solid lightgrey;
-  height: 200px;
-  margin: 10px;
-`
-
-export const WrapSelecBtn = styled.div`
-  display: flex;
-  gap: 20px;
-`
-
 export const SelecPosition = styled.div`
   margin-top: 10px;
   border: 1px solid lightgrey;
   border-radius: 8px;
   background-color: #fff;
   overflow: hidden;
-  /* position: ${(props) => props.position};  */
   width: 250px;
+  position: ${(props) => props.position}; // prop의 값을 받아와서 style 적용
+
     div {
       overflow: hidden; // 옵션리스트 내 말줄임표
       text-overflow: ellipsis;
       white-space: nowrap;
       padding: 8px;
       &:hover {background-color: lightgrey;}
-      /* position: ${(props) => props.position};  */
-
     }
+`
+
+export const OverControl = styled.div`
+  width: 250px;
+  overflow: hidden; // 버튼 내 텍스트 말줄임
+  display: flex;
+  justify-content: space-between; // display와 함께 요소 양쪽 끝 배치
+`
+
+export const Wrap = styled.div`
+  border: 1px solid lightgrey;
+  height: 200px;
+  margin: 10px;
+  overflow: hidden;
+`
+
+export const WrapSelecBtn = styled.div`
+  display: flex;
+  gap: 20px;
 `
