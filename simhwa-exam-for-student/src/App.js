c5b959168a035a126aa13b72de9efc22207bd4fd
abxl1 <etho_818@naver.com>
2023-07-14 20:10:16 +0900

    2sec delay TodoList

--- simhwa-exam-for-student/src/App.js
+++ simhwa-exam-for-student/src/App.js
@@ -20,10 +20,6 @@ function App() {
   const [body, setBody] = useState('');
 
   const onAddTodo = () => {
-    /**
-     * 시험 문제 1.
-     * 이곳에서 추가하기 기능을 구현해주세요.
-     */
 
     dispatch(
       __addToDo({
@@ -36,19 +32,14 @@ function App() {
   };
 
   const onDeleteTodo = (id) => {
-    /**
-     * 시험 문제 2.
-     * 이곳에서 삭제하기 기능을 구현해주세요.
-     */
+
     dispatch(__deleteTodo(id)).then(() => {
       dispatch(__deleteTodo(id));
     });  
   };
 
   const resetInputs = () => {
-    /**
-     * 입력 값을 초기화하고 싶다면 사용하세요.
-     */
+
     setTitle('');
     setBody('');
   };
